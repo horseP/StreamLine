4a962dcfef5dd0374303807a661db22d681e7e6c
Ndu Emmanuel <nduemmanuel4s@gmail.com>
2023-05-22 02:24:35 +0100

    committed

--- intl.js
+++ intl.js
@@ -1,5 +1,6 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
+const sign_up = document.querySelector("#sign-up");
 const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click", () => {
@@ -7,6 +8,9 @@ sign_up_btn.addEventListener("click", () => {
 });
 
 sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+sign_up.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
 
