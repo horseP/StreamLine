b8c57c87dad558c51f1b5e0d3dd3f0e975e8737d
Ndu Emmanuel <nduemmanuel4s@gmail.com>
2023-05-23 11:31:58 +0100

    committed

--- signup.js
+++ signup.js
@@ -2,6 +2,7 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const sign_up = document.querySelector("#sign-up");
+const sign_in = document.querySelector(".loginbtn");
 const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click", () => {
@@ -13,5 +14,15 @@ sign_in_btn.addEventListener("click", () => {
 });
 sign_up.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
+});
+sign_in.addEventListener("click", (event) => {
+  event.preventDefault()
+  const user = 'nduemmanuel@gmail.com'
+  const pass = '11111111'
+  if (user === document.getElementById('loginemail').value && pass === document.getElementById('loginpassword').value){
+   window.location.assign('index.html')
+  }else{
+    alert('incorrect password try again')
+  }
 });
   
